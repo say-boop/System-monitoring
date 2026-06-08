ad6e67e0cb33769dadf7fd643d7591d96f2bf6dd
say-boop <say284821@gmail.com>
2026-06-09 00:12:57 +0500

    ready-metrics

--- app/main.py
+++ app/main.py
@@ -38,16 +38,65 @@ def root(request: Request):
 
 @app.get("/api/health")
 def get_metrics():
+	return get_metrics_all()
+
+
+def get_metrics_all():
 	cpu_percent = psutil.cpu_percent(interval=0.3)
+	cpu_per_core = psutil.cpu_percent(interval=None, percpu=True)
+
 	mem = psutil.virtual_memory()
-	memory_total_gb = round(mem.total / 1024**3, 2)
-	memory_available_gb = round(mem.available / 1024**3, 2)
+	memory_total = round(mem.total / 1024**3, 2)
+	memory_available = round(mem.available / 1024**3, 2)
+	memory_percent = mem.percent
+
+	disk = psutil.disk_usage("/")
+	disk_total = round(disk.total / 1024**3, 2)
+	disk_used = round(disk.used / 1024**3, 2)
+	disk_free = round(disk.free / 1024**3, 2)
+	disk_percent = disk.percent
+
+	net = psutil.net_io_counters()
+	net_mb_sent = net.bytes_sent / 1024 / 1024
+	net_mb_recv = net.bytes_recv / 1024 / 1024
+
+	proc_objects = []
+	for proc in psutil.process_iter(["pid", "name"]):
+		try:
+			proc.cpu_percent(interval=None)
+			proc_objects.append(proc)
+		except (psutil.NoSuchProcess, psutil.AccessDenied):
+			continue
+
+	result = []
+	for proc in proc_objects:
+		try:
+			info = proc.info
+			if info["name"] == "System Idle Process":
+				continue
+			raw_cpu = proc.cpu_percent(interval=None)
+			info["cpu_percent"] = round(raw_cpu / psutil.cpu_count(), 1)
+			result.append(info)
+		except (psutil.NoSuchProcess, psutil.AccessDenied):
+			continue
+
+	top_by_cpu = sorted(result, key=lambda x: x.get("cpu_percent", 0) or 0, reverse=True)[:5]
 
 	return {
-		"cpu": cpu_percent,
-		"memory_total_gb": memory_total_gb,
-		"memory_availalbe_gb": memory_available_gb
+		"cpu_percent": cpu_percent,
+		"cpu_per_core": cpu_per_core,
+		"memory_total": memory_total,
+		"memory_available": memory_available,
+		"memory_percent": memory_percent,
+		"disk_total": disk_total,
+		"disk_used": disk_used,
+		"disk_free": disk_free,
+		"disk_percent": disk_percent,
+		"net_mb_sent": net_mb_sent,
+		"net_mb_recv": net_mb_recv,
+		"top_processes": top_by_cpu
 	}
+		
 
 
 @app.websocket("/ws/metrics")
@@ -55,16 +104,7 @@ async def websocket_metrics(websocket: WebSocket):
 	await websocket.accept()
 	try:
 		while True:
-			cpu_percent = psutil.cpu_percent(interval=0.3)
-			mem = psutil.virtual_memory()
-			memory_total_gb = round(mem.total / 1024**3, 2)
-			memory_available_gb = round(mem.available / 1024**3, 2)
-		
-			content = {
-				"cpu": cpu_percent,
-				"memory_total": memory_total_gb,
-				"memory_available": memory_available_gb
-			}
+			content = get_metrics()
 		
 			await websocket.send_json(content)
 		
--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,15 +1,83 @@
+let cpuHistory = new Array(30).fill(0)
+
+const ctx = document.getElementById("cpu-history-chart").getContext("2d");
+const cpuHistoryChart = new Chart(ctx, {
+	type: "line",
+	data: {
+		labels: [...Array(30).keys()].map(i => i + 1),
+		datasets: [{
+			label: "CPU %",
+			data: cpuHistory,
+			borderColor: "rgb(75, 192, 192)",
+			tension: 0.1
+		}]
+	},
+	options: {
+		scales: {
+			y: {
+				min: 0,
+				max: 100
+			}
+		}
+	}
+});
+
+const ctxCores = document.getElementById("cpu-cores-chart").getContext("2d");
+const cpuCoresChart = new Chart(ctxCores, {
+	type: "bar",
+	data: {
+		labels: [],
+		datasets: [{
+			label: "CPU %",
+			data: [],
+			backgroundColor: "rgba(54, 162, 235, 0.5)"
+		}]
+	}
+});
+
 const socket = new WebSocket("ws://localhost:8000/ws/metrics")
 
 
 socket.onmessage = (event) => {
 	const data = JSON.parse(event.data);
-	const cpu = document.getElementById("cpu-value");
-	const memoryTotal = document.getElementById("memory-total-value");
-	const memoryAvailable = document.getElementById("memory-available-value");
 	
-	cpu.textContent = data.cpu;
-	memoryTotal.textContent = data.memory_total;
-	memoryAvailable.textContent = data.memory_available;
+	document.getElementById("cpu-value").textContent = data.cpu_percent;
+	let coresText = data.cpu_per_core
+    .map((load, i) => `<br>Core ${i + 1}: ${load}%<br>`)
+    .join("-------------------------");
+	document.getElementById("cpu-cores").innerHTML = coresText;
+	
+	document.getElementById("memory-percent").textContent = data.memory_percent;
+	document.getElementById("memory-details").innerHTML = 
+	`<br>Available: ${data.memory_available}GB
+	<br>Total: ${data.memory_total}GB`;
+	
+	document.getElementById("disk-percent").textContent = data.disk_percent
+	document.getElementById("disk-details").innerHTML =
+    `<br>Total: ${data.disk_total}GB
+		<br>Free: ${data.disk_free}GB
+		<br>Used: ${data.disk_used}GB`;
+	
+	document.getElementById("net-sent-recv").innerHTML =
+    `<br>Sent: ${data.net_mb_sent.toFixed(2)}MB
+		<br>Recv: ${data.net_mb_recv.toFixed(2)}MB`;
+	
+	let rows = "";
+	data.top_processes.forEach((proc) => {
+    rows += `<tr><td>${proc.pid}</td><td>${proc.name}</td><td>${proc.cpu_percent}</td></tr>`;
+  });
+	document.getElementById("processes-table").innerHTML = rows;
+	
+	cpuHistory.push(data.cpu_percent);
+	if (cpuHistory.length > 30) {
+		cpuHistory.shift();
+	}
+	cpuHistoryChart.data.datasets[0].data = data.cpuHistory
+	cpuHistoryChart.update()
+	
+	cpuCoresChart.data.labels = data.cpu_per_core.map((_, i) => `Core ${i}`);
+	cpuCoresChart.data.datasets[0].data = data.cpu_per_core;
+	cpuCoresChart.update()
 }
 
 socket.onerror = (error) => {
